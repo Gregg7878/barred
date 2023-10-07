9c5f4fd5ebefa1eccb7c8f15f3bd9e45b2a031ae
Alex <alexkimanthi92@gmail.com>
2023-10-07 10:18:40 +0300

    updated sessions

--- app/controllers/customer_sessions_controller.rb
+++ app/controllers/customer_sessions_controller.rb
@@ -1,2 +1,34 @@
 class CustomerSessionsController < ApplicationController
+
+    def create 
+        customer = Customer.find_by(email: params[:email])
+        if customer&.authenticate(params[:password])
+            session[:customer_id] = customer.id 
+            render json: customer     
+        
+        else
+            errors = ["Invalid email or password"]
+            # if customer.nil?
+            #     errors << 'invalid password'
+            # else 
+            #     errors << 'invalid email'
+            # end
+            render json: { errors: errors }, status: :unauthorized 
+        end 
+    end
+
+    def destroy      
+       if logged_in?
+        session.delete :customer_id
+        head :no_content 
+       else
+        errors = ['Not logged in']
+        render json: { errors: errors }, status: :unauthorized
+       end
+    end 
+
+    private 
+    def logged_in?
+           session.include? :customer_id
+    end 
 end
--- app/controllers/organizer_sessions_controller.rb
+++ app/controllers/organizer_sessions_controller.rb
@@ -1,2 +1,35 @@
 class OrganizerSessionsController < ApplicationController
+
+    def create 
+        organizer = Organizer.find_by(email: params[:email])
+        if organizer&.authenticate(params[:password])
+            session[:organizer_id] = organizer.id 
+            render json: organizer    
+        
+        else
+            errors = ["Invalid email or password"]
+            # if organizer.nil?
+            #     errors << 'invalid password'
+            # else 
+            #     errors << 'invalid email'
+            # end
+            render json: { errors: errors }, status: :unauthorized 
+        end 
+    end
+
+    def destroy      
+       if logged_in?
+        session.delete :organizer_id
+        head :no_content 
+       else
+        errors = ['Not logged in']
+        render json: { errors: errors }, status: :unauthorized
+       end
+    end 
+
+    private  
+    def logged_in?
+        #    !session[:user_id].nil?
+           session.include? :organizer_id
+    end 
 end
