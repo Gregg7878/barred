5518f2359c8c827f0c11c50ec96e2deb0ff62284
Ken <kabutukenneth3@gmail.com>
2023-10-07 21:19:29 +0300

    create ticket migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_07_175045) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tickets", force: :cascade do |t|
+    t.string "ticket_type"
+    t.decimal "price"
+    t.datetime "purchase_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
