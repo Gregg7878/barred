21b52e8795369a71d33eff19b4d0e47baa7e000c
Alex <alexkimanthi92@gmail.com>
2023-10-07 10:00:23 +0300

    updated sessions

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,3 +1,49 @@
 class CustomersController < ApplicationController
+
+    def index
+        customers = Customer.all 
+        render json: customers  
+      end 
+    
+      def show 
+        customer = Customer.find_by(id: session[:customer_id]) 
+        if customer
+            render json: customer, status: :created 
+        else
+            render json: { error: "not authorized here" }, status: :unauthorized 
+        end 
+      end
+    
+      def create 
+        customer = Customer.new(customer_params)
+        if customer.save 
+          session[:customer_id] = customer.id 
+          render json: customer, status: :created 
+        else
+          render json: { error: "Not created"}, status: :unauthorized 
+        end
+      end
+    
+      def update  
+        customer = Customer.find_by(id: params[:customer_id])
+        
+        if customer.update!(customer_params)
+          session[:customer_id] = customer.id 
+          render json: customer
+        else
+          render json: { error: "Not updated"}, status: :unauthorized 
+        end
+      end
+    
+      def destroy 
+        customer = Customer.find_by(id: params[:customer_id])
+        customer.delete 
+        head :no_content 
+      end
+    
+      private 
+      def customer_params 
+        params.permit(:first_name, :last_name :email, :age)  
+      end
 end
  
--- app/controllers/organizers_controller.rb
+++ app/controllers/organizers_controller.rb
@@ -1,3 +1,23 @@
 class OrganizersController < ApplicationController
+
+    def index
+        organizers = Organizer.all 
+        render json: organizers 
+      end
+    
+      def show 
+        organizer = Organizer.find_by(id: session[:organizer_id]) 
+        if organizer
+            render json: organizer 
+        else
+            render json: { error: "not authorized here" }, status: :unauthorized 
+        end 
+      end
+    
+    
+      private
+      def organizer_params 
+        params.permit(:first_name, :last_name :email, :age) 
+      end
 end
  
