6816f308a3499aebcdef9b69c2683428fbb75467
Alex <alexkimanthi92@gmail.com>
2023-10-07 15:09:00 +0300

    updated validations on organizer model

--- app/models/organizer.rb
+++ app/models/organizer.rb
@@ -1,3 +1,8 @@
 class Organizer < ApplicationRecord
     has_secure_password 
+
+    validates :email, presence: true, uniqueness: true 
+    validates :age, presence: true, numericality: { greater_than_or_equal_to: 18 }
+    validates :first_name, presence: true
+    validates :last_name, presence: true
 end
