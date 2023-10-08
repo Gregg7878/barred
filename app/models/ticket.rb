abad6d97b2baff1409bf8b5b3ae071287dfdac3b
Ken <kabutukenneth3@gmail.com>
2023-10-08 07:37:28 +0300

    add validations to ticket model

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,7 +3,8 @@ class Ticket < ApplicationRecord
   belongs_to :customer
 
 
-  validates :ticket_type, inclusion: { in: ['MVP', 'Early Booking', 'Regular'] }
-  validates :price, numericality: { greater_than_or_equal_to: 0 }
-  
+  validates :ticket_type, presence: true, inclusion: { in: ['MVP', 'Early Booking', 'Regular'] }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :purchase_date, presence: true
+
 end
