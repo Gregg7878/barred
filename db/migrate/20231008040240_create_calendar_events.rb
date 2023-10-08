de122ebcb10fcc7d49218ded1e6213f48774d1ff
Ken <kabutukenneth3@gmail.com>
2023-10-08 07:05:55 +0300

    run migrations

--- db/migrate/20231008040240_create_calendar_events.rb
+++ db/migrate/20231008040240_create_calendar_events.rb
@@ -1,8 +1,8 @@
 class CreateCalendarEvents < ActiveRecord::Migration[7.0]
   def change
     create_table :calendar_events do |t|
-      t.references :event, null: false, foreign_key: true
-      t.references :user, null: false, foreign_key: true
+      # t.references :event, null: false, foreign_key: true
+      # t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_07_175045) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_08_040240) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "calendar_events", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tickets", force: :cascade do |t|
     t.string "ticket_type"
