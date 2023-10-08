921852dadcc28cea32ce28bcfaf559596d31a6fb
Ken <kabutukenneth3@gmail.com>
2023-10-08 07:49:54 +0300

    add validations to calendarEvent model

--- app/models/calendar_event.rb
+++ app/models/calendar_event.rb
@@ -1,4 +1,6 @@
 class CalendarEvent < ApplicationRecord
   belongs_to :event
   belongs_to :user
+
+  validates_uniqueness_of :event_id, scope: :user_id, message: 'has already been added to your calendar'
 end
