adb0122627ffb89ac5eccf943e0687245da0685e
Ken <kabutukenneth3@gmail.com>
2023-10-08 07:17:24 +0300

    add crud action to calendarevents controller

--- app/controllers/calendar_events_controller.rb
+++ app/controllers/calendar_events_controller.rb
@@ -1,3 +1,8 @@
 class CalendarEventsController < ApplicationController
     before_action :authenticate_user
+
+    # Display a list of calendar events for the current user
+    def index
+        @calendar_events = current_user.calendar_events
+    end
 end
