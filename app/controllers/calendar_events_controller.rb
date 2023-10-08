e83add52f910f2495f8aae5d409e36d2501576c6
Ken <kabutukenneth3@gmail.com>
2023-10-08 07:21:09 +0300

    add crud action to calendarevents controller

--- app/controllers/calendar_events_controller.rb
+++ app/controllers/calendar_events_controller.rb
@@ -5,4 +5,16 @@ class CalendarEventsController < ApplicationController
     def index
         @calendar_events = current_user.calendar_events
     end
+
+    def create
+        @calendar_event = current_user.calendar_events.build(calendar_event_params)
+
+        if @calendar_event.save
+            render json: @calendar_event, status: :created
+        else 
+            render json: @calendar_event.errors, status: :unprocessable_entity
+        end
+    end
+
+    
 end
