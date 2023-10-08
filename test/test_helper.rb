aeb5a0c6739ead339f6cb9947eb195d58939b349
Ken <kabutukenneth3@gmail.com>
2023-10-08 07:56:22 +0300

    create test for ticket model

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,11 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
+require 'test_helper'
 
 class ActiveSupport::TestCase
-  # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
-
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
-
-  # Add more helper methods to be used by all tests here...
+  test 'should not save ticket without ticket_type' do
+    ticket = Ticket.new
+    assert_not ticket.save, 'Saved the ticket without a ticket_type'
+  end
 end
