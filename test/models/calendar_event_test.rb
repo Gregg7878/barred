de215807c5a220f5bab29ffadce18284b6ae3225
Ken <kabutukenneth3@gmail.com>
2023-10-08 08:03:55 +0300

    create test for calendarEvent model

--- test/models/calendar_event_test.rb
+++ test/models/calendar_event_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class CalendarEventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should not allow adding the same event to the calendar multiple times' do
+    user = users(:one)  # Replace with your fixture or create a user as needed
+    event = events(:one)  # Replace with your fixture or create an event as needed
+
+    # Create a calendar event for the same user and event
+    calendar_event = CalendarEvent.new(user: user, event: event)
+
+    # Attempt to save the calendar event
+    assert_not calendar_event.save, 'Allowed adding the same event to the calendar multiple times'
+  end
 end
